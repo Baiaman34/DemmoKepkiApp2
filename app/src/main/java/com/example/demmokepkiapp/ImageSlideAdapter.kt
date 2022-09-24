b0ee4deea951dab68f10cbbdae0004a70eec1473
Lenovo <krasavabaia174@gmail.com>
2022-09-24 20:50:50 +0300

    first commit

---
+++ app/src/main/java/com/example/demmokepkiapp/ImageSlideAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.demmokepkiapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class NumberAdapter()
+
+    }
+}
---
+++ app/src/main/java/com/example/demmokepkiapp/ImageSlideFragment.kt
@@ -0,0 +1,4 @@
+package com.example.demmokepkiapp
+
+class ImageSlideFragment {
+}
